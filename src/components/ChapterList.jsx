ba7f4953cb30bfcda05c54bdf4eb721ab2cb7821
heliobei <suporte@beieducacaopedagogico.com>
2025-06-10 14:48:33 -0300

    Update ChapterList.jsx

--- src/components/ChapterList.jsx
+++ src/components/ChapterList.jsx
@@ -5,8 +5,9 @@ const chapters = [
   { id: 1, title: "Qual é o meu sonho?", teacherPdf: "/assets/pdfs/Educador_Capitulo_1_6o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_1_6o.pdf" },
   { id: 2, title: "O que você precisa comprar no mercado?", teacherPdf: "/assets/pdfs/Educador_Capitulo_2_6o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_2_6o.pdf" },
   { id: 3, title: "As tentações do mercado... Me segura pra eu não gastar!", teacherPdf: "/assets/pdfs/Educador_Capitulo_3_6o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_3_6o.pdf" },
-  { id: 4, title: "Alcançando objetivos em família", teacherPdf: "/assets/pdfs/Educador_Capitulo_4_6o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_4_6o.pdf" },
-  // ... mais capítulos
+  { id: 4, title: "Alcançando objetivos em família", teacherPdf: "/assets/pdfs/Educador_Capitulo_04_6o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_04_6o.pdf" },
+  { id: 5, title: "Poupando o bolso e o meio ambiente", teacherPdf: "/assets/pdfs/Educador_Capitulo_05_6o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_05_6o.pdf" },
+  // ... mais capítulos  
 ];
 
 const ChapterList = () => {
